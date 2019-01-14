fb75b4d401e0e4487dc2ae607babab5f831335d0
BadPixxel <eshop.bpaquier@gmail.com>
2019-01-14 06:01:53 +0100

    Add WebHook Tests

---
+++ src/Controller/WebHooksController.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2019 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Connectors\MailChimp\Controller;
+
+use Psr\Log\LoggerInterface;
+use Splash\Bundle\Models\AbstractConnector;
+use Splash\Connectors\MailChimp\Objects\ThirdParty;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Splash MailChimp WebHooks Actions Controller
+ */
+class WebHooksController extends Controller
+{
+    /**
+     * Execute WebHook Actions for A MailChimp Connector
+     *
+     * @param Request           $request
+     * @param AbstractConnector $connector
+     *
+     * @return JsonResponse
+     */
+    public function indexAction(LoggerInterface $logger, Request $request, AbstractConnector $connector)
+    {
+        //====================================================================//
+        // For Mailchimp Ping GET
+        if ($request->isMethod('GET')) {
+            $logger->error(__CLASS__ . '::'.__FUNCTION__. ' MailChimp Ping.', $request->attributes->all());
+
+            return new JsonResponse(array( 'success' => true, 'ping' => 'pong' ));
+        }
+        
+        //====================================================================//
+        // Read Request Parameters
+        $type = $request->request->get('type');
+        $data = $request->request->get('data');
+        
+        //====================================================================//
+        // Log MailChimp Request
+        $logger->info(__CLASS__ . '::'.__FUNCTION__. ' WebHook Type ' . $type . '.', (is_array($data) ? $data : array()));
+        
+        //====================================================================//
+        // Verify Impacted List is Node Selected List
+        if ($connector->getParameter('ApiList') != $data["list_id"]) {
+            $logger->error(__CLASS__ . '::'.__FUNCTION__. ' MailChimp Wrong List.', $request->attributes->all());
+
+            return new JsonResponse(array( 'success' => true, 'ping' => 'pong' ));
+        }
+        
+        //==============================================================================
+        // Detect Change Parameters
+        if (("unsubscribe" == $type) && ('delete' == $data["action"])) {
+            $action     =   SPL_A_DELETE;
+            $objectId   =   ThirdParty::hash($data["email"]);
+        } elseif (in_array($type, array("subscribe", "unsubscribe", "profile" ), true)) {
+            $action     =   SPL_A_UPDATE;
+            $objectId   =   ThirdParty::hash($data["email"]);
+        } elseif (in_array($type, array("upemail"), true)) {
+            //====================================================================//
+            // Update Object Id as Changed by this Request (Email Modified)
+            $connector->objectIdChanged(
+                "ThirdParty",
+                 ThirdParty::hash($data["old_email"]),
+                 ThirdParty::hash($data["new_email"])
+            );
+            $action     =   SPL_A_UPDATE;
+            $objectId   =   ThirdParty::hash($data["new_email"]);
+        } elseif (in_array($type, array("cleaned"), true)) {
+            $action     =   SPL_A_DELETE;
+            $objectId   =   ThirdParty::hash($data["email"]);
+        } else {
+            return new JsonResponse(array( 'success' => true, 'ping' => 'pong' ));
+        }
+        
+        //==============================================================================
+        // Commit Changes
+        $connector->commit('ThirdParty', $objectId, $action, "MailChimp API", "Member Updated");
+        
+        //==============================================================================
+        // Send Response
+        return new JsonResponse(array('success' => true, 'type' => $type));
+    }
+}
---
+++ tests/Controller/S01WebHookTest.php
@@ -0,0 +1,217 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2019 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Connectors\MailChimp\Tests\Controller;
+
+use Splash\Connectors\MailChimp\Objects\ThirdParty;
+use Splash\Connectors\MailChimp\Services\MailChimpConnector;
+use Splash\Tests\Tools\TestCase;
+
+class S01WebHookTest extends TestCase
+{
+    const PING_RESPONSE = '{"success":true,"ping":"pong"}';
+    const MEMBER = "ThirdParty";
+    const FAKE_EMAIL = "fake@exemple.com";
+   
+    /**
+     * Test WebHook For Ping
+     */
+    public function testWebhookPing()
+    {
+        //====================================================================//
+        // Load Connector
+        $connector = $this->getConnector("mailchimp");
+        $this->assertInstanceOf(MailChimpConnector::class, $connector);
+        
+        //====================================================================//
+        // Touch Url
+        $this->assertPublicActionWorks($connector);
+        $this->assertEquals(self::PING_RESPONSE, $this->getResponseContents());
+        
+        $this->assertPublicActionWorks($connector, null, array(), "POST");
+        $this->assertEquals(self::PING_RESPONSE, $this->getResponseContents());
+    }
+
+    /**
+     * Test WebHook with Errors
+     */
+    public function testWebhookErrors()
+    {
+        //====================================================================//
+        // Load Connector
+        $connector = $this->getConnector("mailchimp");
+        $this->assertInstanceOf(MailChimpConnector::class, $connector);
+
+        //====================================================================//
+        // GOOD LIST ID BUT GET METHOD
+        //====================================================================//
+
+        //====================================================================//
+        // Prepare Request
+        $data  = array(
+            "data" => array(
+                "list_id" => $connector->getParameter("ApiList")
+            )
+        );
+
+        //====================================================================//
+        // Touch Url
+        $this->assertPublicActionWorks($connector, null, $data, "GET");
+        $this->assertEquals(self::PING_RESPONSE, $this->getResponseContents());
+        
+        //====================================================================//
+        // WRONG LIST ID
+        //====================================================================//
+
+        //====================================================================//
+        // Prepare Request
+        $data2  = array(
+            "data" => array(
+                "list_id" => "ThisIsWrong"
+            )
+        );
+        
+        //====================================================================//
+        // Touch Url
+        $this->assertPublicActionWorks($connector, null, $data2, "POST");
+        $this->assertEquals(self::PING_RESPONSE, $this->getResponseContents());
+        
+        //====================================================================//
+        // GOOD LIST ID BUT WRONG TYPE
+        //====================================================================//
+
+        //====================================================================//
+        // Prepare Request
+        $data3  = array(
+            "type" => "ThisIsWrong",
+            "data" => array(
+                "list_id" => $connector->getParameter("ApiList")
+            )
+        );
+
+        //====================================================================//
+        // Touch Url
+        $this->assertPublicActionWorks($connector, null, $data3, "POST");
+        $this->assertEquals(self::PING_RESPONSE, $this->getResponseContents());
+    }
+
+    /**
+     * Test WebHook Member Delete
+     *
+     * @dataProvider webHooksInputsProvider
+     */
+    public function testWebhookRequest(string $type, array $data, string $objectType, string $action, string $objectId)
+    {
+        //====================================================================//
+        // Load Connector
+        $connector = $this->getConnector("mailchimp");
+        $this->assertInstanceOf(MailChimpConnector::class, $connector);
+        
+        //====================================================================//
+        // Prepare Request
+        $post  = array(
+            "type" => $type,
+            "data" => array_replace_recursive(
+                    array("list_id" => $connector->getParameter("ApiList")),
+                    $data
+                )
+        );
+
+        //====================================================================//
+        // Touch Url
+        $this->assertPublicActionWorks($connector, null, $post, "POST");
+        $this->assertEquals(
+                json_encode(array("success" => true, "type" => $type)),
+                $this->getResponseContents()
+            );
+
+        //====================================================================//
+        // Verify Response
+        $this->assertIsLastCommited($action, $objectType, $objectId);
+    }
+
+    /**
+     * Generate Fake Inputs fro WebHook Requets
+     *
+     * @return array
+     */
+    public function webHooksInputsProvider()
+    {
+        return array(
+            //====================================================================//
+            // Subscribe
+            array(
+                "subscribe",
+                array("email" => self::FAKE_EMAIL),
+                self::MEMBER,
+                SPL_A_UPDATE,
+                ThirdParty::hash(self::FAKE_EMAIL)
+            ),
+
+            //====================================================================//
+            // Update Profile
+            array(
+                "profile",
+                array("email" => self::FAKE_EMAIL),
+                self::MEMBER,
+                SPL_A_UPDATE,
+                ThirdParty::hash(self::FAKE_EMAIL)
+            ),
+            
+            //====================================================================//
+            // Update Email
+            array(
+                "upemail",
+                array(
+                    "old_email" => "old." . self::FAKE_EMAIL,
+                    "new_email" => self::FAKE_EMAIL
+                ),
+                self::MEMBER,
+                SPL_A_UPDATE,
+                ThirdParty::hash(self::FAKE_EMAIL)
+            ),
+            
+            //====================================================================//
+            // Unsubscribe & No Delete
+            array(
+                "unsubscribe",
+                array("email" => self::FAKE_EMAIL),
+                self::MEMBER,
+                SPL_A_UPDATE,
+                ThirdParty::hash(self::FAKE_EMAIL)
+            ),
+            
+            //====================================================================//
+            // Unsubscribe & Delete
+            array(
+                "unsubscribe",
+                array("email" => self::FAKE_EMAIL, "action" => "delete"),
+                self::MEMBER,
+                SPL_A_DELETE,
+                ThirdParty::hash(self::FAKE_EMAIL)
+            ),
+
+            //====================================================================//
+            // Cleaned
+            array(
+                "cleaned",
+                array("email" => self::FAKE_EMAIL),
+                self::MEMBER,
+                SPL_A_DELETE,
+                ThirdParty::hash(self::FAKE_EMAIL)
+            ),
+        );
+    }
+}
